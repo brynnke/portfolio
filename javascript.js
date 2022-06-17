ea7df2507ce349c0c6ffb547e600bf19f8a7e7cd
brynnke <100329799+brynnke@users.noreply.github.com>
2022-06-16 20:20:04 -0500

    all files plus added second project and the other repo was messed up

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function projectClicked(input) {
+    var input = input;
+
+    if (input == "challenge-1") {
+        window.open("https://github.com/brynnke/challengeone/");
+    } else if (input == "run-buddy") {
+        window.open("https://github.com/brynnke/runbuddy/");
+    } else if (input == 'password-generator') {
+        window.open("https://brynnke.github.io/passwordgen/");
+    } else if (input == 'api-code-quiz') {
+        window.open("https://brynnke.github.io/jsquiz/");
+    } else if (input == "work-day-scheduler"){
+        window.open("https://brynnke.github.io/work-day-sched/");
+    } else if (input == "project 1"){
+        window.open ("https://omarwright07.github.io/music-guru/");    
+    } else if (input == "full-stack project"){
+        window.open("https://unleashed-pets-finder.herokuapp.com/");
+    } else {
+        return true; 
+    }
+};
